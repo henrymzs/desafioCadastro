d8a582c2bbc5ab010e4152917ba5e9e4bd74de9c
henrymzs <henrykaua21@gmail.com>
2026-02-28 15:53:29 -0300

    refactor(main): update application bootstrap with Dependency Injection

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,22 @@
 import java.io.IOException;
 
-import controller.PetController;
+import controller.PetRegistrationController;
+import repository.PetRepository;
 import service.FileReaderService;
-import service.PetService;
 import view.ConsoleInteraction;
-import view.PetFormView;
 
 public class Main {
   public static void main(String[] args) throws IOException, InterruptedException {
 
-    PetService service = new PetService();
-    PetFormView view = new PetFormView();
     FileReaderService fileReader = new FileReaderService();
 
-    PetController controller = new PetController(service, view, fileReader);
-    ConsoleInteraction interaction = new ConsoleInteraction(controller);
-    
+    PetRepository repository = new PetRepository();
+
+    PetRegistrationController registrationController = new PetRegistrationController(repository);
+
+    ConsoleInteraction interaction = new ConsoleInteraction(registrationController);
+    fileReader.getAllQuestions();
+
     interaction.start();
   }
 }
